141514247dcd178e1702eac9fc45381b0c46afd7
Vyacheslav Dobranov <vy.dobranov@yandex.ru>
2025-12-06 08:53:48 +0500

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -11,26 +11,10 @@ WASM_WHEEL_URL = "https://ifcopenshell.github.io/wasm-wheels/ifcopenshell-0.8.2+
 WASM_INSTALL_FLAG = "ifcopenshell_wasm_installed_v1"
 
 
-async def install_and_import_ifcopenshell():
-	try:
-		status.value = "Устанавливаю ifcopenshell..."
-		page.update()
-		await micropip.install(WASM_WHEEL_URL)
-	except Exception as e:
-		print(f"Ошибка установки: {e}")
-	try:
-		import ifcopenshell
-		ifc_file = ifcopenshell.file()
-		status.value += "\nСоздан пустой IFC-файл."
-		page.update()
-	except:
-		pass
+
 
 
 async def main(page: ft.Page):
-	await install_and_import_ifcopenshell()
-
-
 	page.title = "IFC-IDS Report"
 
 	header = ft.Text(
@@ -46,14 +30,29 @@ async def main(page: ft.Page):
 	)
 
 	status = ft.Text("Статус: ")
-	page.add(status)
-
 	page.add(
 		header,
 		status,
 		ifc_picker_btn,
 		results
 	)
+	async def install_and_import_ifcopenshell():
+		try:
+			status.value = "Устанавливаю ifcopenshell..."
+			page.update()
+			await micropip.install(WASM_WHEEL_URL)
+		except Exception as e:
+			print(f"Ошибка установки: {e}")
+		try:
+			import ifcopenshell
+			ifc_file = ifcopenshell.file()
+			status.value += "\nСоздан пустой IFC-файл."
+			page.update()
+		except:
+			pass
+		await install_and_import_ifcopenshell()
+
+
 
 
 ft.app(target=main)
